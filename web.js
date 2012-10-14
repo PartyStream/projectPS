c6411238eb15eeba6c93ed0fec095bd0e2ef152d
Salvatore D'Agositno <toto19@gmail.com>
2012-10-14 19:03:22 -0400

    changed for start of REST setup

--- web.js
+++ web.js
@@ -1,11 +1,27 @@
-var server          = require('./server');
-var router          = require('./router');
-var requestHandlers = require('./requestHandlers');
+var application_root = __dirname,
+    express          = require("express"),
+    path             = require("path"),
+    mongoose         = require('mongoose');
 
-var handle        = {}
-handle["/"]       = requestHandlers.start;
-handle["/start"]  = requestHandlers.start;
-handle["/upload"] = requestHandlers.upload;
+var app = express.createServer();
 
+// Database
 
-server.start(router.route, handle);
+mongoose.connect('mongodb://localhost/photostream');
+
+// Config
+app.configure(function () {
+  app.use(express.bodyParser());
+  app.use(express.methodOverride());
+  app.use(app.router);
+  app.use(express.static(path.join(application_root, "public")));
+  app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
+});
+
+app.get('/api', function (req, res) {
+  res.send('PS API is running');
+});
+
+// Launch server
+
+app.listen(5000);
