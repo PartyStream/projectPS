05d66d0a2f21b5314aae99f83affa92ad82633da
Salvatore D'Agositno <toto19@gmail.com>
2012-12-12 22:40:16 -0500

    inserting the picture record into the DB

--- api/pictures.js
+++ api/pictures.js
@@ -18,20 +18,52 @@
 +   \author Salvatore D'Agostino
 +   \date  2012-12-09 15:41
 +   \param response   (HTTP)    The response to return to the client
-+   \param eventName  (STRING)  The name of the event for this file
++   \param eventId    (STRING)  The name of the event for this file
 +   \param creator    (STRING)  The ID of the creator
-+   \param fileName   (STRING)  The name of the file
++   \param picture    (FILE)    The file that was uploaded
 +   \param data       (BIN)     The contents of the file
 +   \param s3         (CONN)    The contents of the file
 +   \param client     (PSQL)    The PSQL client objects
 +
 +   \return True if pass, False otherwise
 **/
-function createPicture(response,eventName,creator,fileName,data,s3)
+function createPicture(response,eventId,creator,picture,s3,client)
 {
     var awsS3 = require('./amazonS3');
 
+    // TODO create record in DB for picture and get back ID
+    console.log('Inserting image into DB');
+
+    query = client.query({
+      name: 'insert picture',
+      text: "INSERT INTO pictures (name,owner,date_created) values ($1,$2,current_timestamp) RETURNING id",
+      values: [picture.name, creator]
+    });
+
+    query.on('error',function(err) {
+        console.log('DB Error Caught: '+ err);
+        // Send response to client
+        response.writeHead(200,{"Content-Type":"text/plain"});
+        response.write("Could not upload picture");
+        response.end();
+    });
+
+    var pictureId;
+    // return the id of the picture inserted
+    query.on('row', function(row) {
+        pictureId = row;
+        console.log("Picture ID: "+pictureId);
+    });
+
+
+    // TODO use ID as filename to upload to S3
+
     // code
+
+    // Send response to client
+    response.writeHead(200,{"Content-Type":"text/plain"});
+    response.write("Picture Uploaded!");
+    response.end();
     
 }// END function createPicture
 exports.createPicture = createPicture;
