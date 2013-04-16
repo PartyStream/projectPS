6af0ef1b8c2829709c43608a56c160ec1004d015
Salvatore D'Agositno <toto19@gmail.com>
2013-04-15 22:12:54 -0400

    started create session function in authenticate model updates SQL schema with session table

--- api/authenticate.js
+++ api/authenticate.js
@@ -9,3 +9,35 @@
 //  Copyright 2013. All rights reserved.
 //
 **/
+
+/**
++   \brief createSession
++
++       This function is responsible for authentication and creating a session for a user
++
++   \author Salvatore D'Agostino
++   \date  2013-04-15 21:23
++   \param response             The response to return to the client
++   \param userObject   (JSON)  The user data to use for authentication
++   \param client       (PSQL)  PSQL client object
++
++   \return Auth token if authentication successful, False otherwise
+**/
+function createSession(response,userObject,client)
+{
+    var credentials = JSON.parse(userObject);
+
+    console.log('Authenticating user: ' + credentials.username);
+
+    query = client.query({
+        name: 'authenticate',
+        text: 'SELECT COUNT(id) from users where username = $1 AND password = $2;',
+        values: [user.username, user.password]
+    });
+
+    // TODO: If user exists, create session
+    // TODO: else throw error
+
+
+}// END function createSession
+exports.createSession = createSession;
