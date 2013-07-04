149d1cc984390389363ef81d059a3ea75c096a14
Salvatore D'Agositno <toto19@gmail.com>
2013-07-03 21:46:27 -0400

    fix CORS... hopefully #8

--- api/web.js
+++ api/web.js
@@ -59,7 +59,7 @@ app.configure(function () {
 +++                                             +++
 +++++++++++++++++++++++++++++++++++++++++++++++++++
 **/
-app.all('/', function(req, res, next) {
+app.all('/*', function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "X-Requested-With");
   next();
