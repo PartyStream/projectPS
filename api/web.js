92f5e593885dc7419fdbc64363803598c1b8224c
Salvatore D'Agositno <toto19@gmail.com>
2013-07-03 21:09:30 -0400

    another attempt to fix CORS

--- api/web.js
+++ api/web.js
@@ -52,14 +52,6 @@ app.configure(function () {
     app.delete(/object/:id)    (remove)
 */
 
-app.get('/api', function (req, res) {
-  res.send('PS API is running');
-});
-app.get('/', function (req, res) {
-  res.send('PS API is running');
-});
-
-
 /**
 +++++++++++++++++++++++++++++++++++++++++++++++++++
 +++                                             +++
@@ -72,6 +64,13 @@ app.all('/', function(req, res, next) {
   res.header("Access-Control-Allow-Headers", "X-Requested-With");
   next();
  });
+
+app.get('/api', function (req, res) {
+  res.send('PS API is running');
+});
+app.get('/', function (req, res) {
+  res.send('PS API is running');
+});
 
 /**
 +++++++++++++++++++++++++++++++++++++++++++++++++++
